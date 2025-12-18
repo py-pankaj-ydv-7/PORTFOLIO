016a25a403db3af6a6ffaad6dd72397b4744a01e
Pankaj Yadav <pankajyadav851685@gmail.com>
2025-12-18 12:48:00 +0530

    contact form  id done

--- pyportfolio/src/Components/Contact/Contect.jsx
+++ pyportfolio/src/Components/Contact/Contect.jsx
@@ -4,8 +4,32 @@ import theme_pattern from '../../assets/theme_pattern.svg'
 import mail_icon from '../../assets/mail_icon.svg'
 import location_icon from '../../assets/location_icon.svg'
 import call_icon from '../../assets/call_icon.svg'
+// import { useState } from 'react'
 
 function Contect() {
+    const onSubmit = async (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", "3762ee96-76ae-4902-8626-375bb5a331bb");
+
+    const object = Object.fromEntries(formData);
+    const json = JSON.stringify(object);
+
+    const res = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json"
+      },
+      body: json
+    }).then((res) => res.json());
+
+    if (res.success) {
+      alert(res.message);
+    }
+  };
+
   return (
     <div id='contact' className='contact'>
         <div className='contact-title'>
@@ -28,7 +52,7 @@ function Contect() {
                     </div>
                 </div>
             </div>
-            <form className='contact-right'>
+            <form onSubmit={onSubmit} className='contact-right'>
                 <label htmlFor="">Your Name</label>
                 <input type="text" placeholder='enter your name' name='name' />
                 <label htmlFor="">Your Email</label>
